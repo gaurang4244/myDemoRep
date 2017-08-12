67d7110c6b59f70bd24cd290a735a89118e9e6ee
gaurang patel <pgp8758@gmail.com>
2017-08-12 17:50:37 +0530

    first commit

---
+++ firstGame/firstGame/FirstViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FirstViewController.swift
+//  firstGame
+//
+//  Created by Gaurang Patel on 17/07/17.
+//  Copyright © 2017 Gaurang Patel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
